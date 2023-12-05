b1ff0934494feba8da736bee4ee3a392640c4c56
welcomevideogame <flory_call0d@icloud.com>
2023-12-04 23:29:59 -0500

    Basic Logging In

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,51 +1,44 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use serde::{Deserialize, Serialize};
 use tauri::State;
 use std::sync::Mutex;
 
-
-#[derive(Deserialize, Serialize)]
-struct DBManager{
-    i: i32,
-}
-
-impl DBManager{
-    fn new() -> Self{
-        DBManager {
-            i: 10,
-        }
-    }
-
-    fn get_connection(&self) -> bool {
-        false
-    }
-
-    fn get_data(&self) -> String{
-        String::from("This is a test")
-    }
-
-    fn log_in(&self) -> bool{
-        true
-    }
-}
+mod app;
+mod types;
+mod utils;
 
 #[tauri::command]
-fn test(manager: State<Mutex<DBManager>>) -> bool {
-    manager.lock().unwrap().log_in()
+async fn authenticate<'a>(
+    tool: State<'_, Mutex<app::App>>, 
+    id: u16, 
+    password: &'a str
+) -> Result<bool, String> {
+    match tool.lock() {
+        Ok(app) => {
+            println!("{}", app.authenticate_employee(id, password));
+            match app.authenticate_employee(id, password) {
+                true => Ok(true),
+                false => Err("Authentication failed".to_string()),
+            }
+        },
+        Err(_) => Err("Failed to acquire lock".to_string()),
+    }
 }
 
+
 #[tauri::command]
-fn log_in(username: &str, password: &str) -> bool{
-    username == password
+fn test() -> bool{
+    true
 }
+
 
 fn main() {
-    let db_manager = Mutex::new(DBManager::new());
+    let app = Mutex::new(app::App::new());
+    app.lock().unwrap().run();
 
     tauri::Builder::default()
-        .manage(db_manager)    
-        .invoke_handler(tauri::generate_handler![log_in, test])
+        .manage(app)    
+        .invoke_handler(tauri::generate_handler![authenticate, test])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
